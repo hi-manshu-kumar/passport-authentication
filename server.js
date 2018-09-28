2dc8bfa73806e542b4628151fe70caa24cbaf747
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-09-28 14:13:37 +0530

    mongoose config added

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 const port = process.env.PORT || 1234;
 const app = express();
+const passport = require('passport');
+const mongoose = require('mongoose');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(passport,initilize());
+app.use(passport.session());
+
+app.get('/success', (req,res) => res.send("Welcome " + req.query.username + "!!"));
+app.get('/error', (req ,res) => res.send("error logging in"));
+
+
+passport.deserializeUser(function (id, cb){
+    User.findById(id, function(err, user){
+        cb(err, user);
+    });
+})
+
+mongoose.connect('mongodb://localhost/MyDatabase');
+
+const Schema = mongoose.Schema;
+
+const UserDetail = new Schema({
+    username: String,
+    password: String
+});
+
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
 
 // app.get('/', (req, res)=> {
 //     res.sendFile('index.html', { root: __dirname})
